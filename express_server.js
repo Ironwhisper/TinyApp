1823bb9b6ab62bb257ddf4131ec3c6874f680f47
vagrant <stan.solomean@gmail.com>
2019-01-18 00:11:04 +0000

    added comments and logout function!

--- express_server.js
+++ express_server.js
@@ -35,69 +35,81 @@ function generateRandomString() {
   return result;
 }
 
-
+//INDEX PAGE
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"]
+    };
   res.render("urls_index", templateVars);
 });
 
+//URLS_NEW
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+  username: req.cookies["username"],
+};
+  res.render("urls_new", templateVars);
 });
 
+//LOGIN
 app.post("/login", (req, res) => {
-  res.cookie(req.body.login, '1')
-
+  res.cookie("username", req.body.login)
   res.redirect("/urls");
 });
 
+//LOGOUT
+app.post("/logout", (req, res) => {
+  res.clearCookie("username")
+  // req.cookies.name
+  res.redirect("urls");
+})
+
+//REDIRECT
 app.post("/urls", (req, res) => {
-
   let randomId = generateRandomString();
-
   urlDatabase.push({id: randomId, long: req.body.longURL})
   let target_url = "/urls/" + randomId
   res.redirect(target_url)
 });
 
-
-
+//SHOW URL
 app.get("/urls/:id", (req, res) => {
   let templateVars = {
     shortURL: req.params.id,
-    urls: urlDatabase
+    urls: urlDatabase,
+    username: req.cookies["username"]
   };
   res.render("urls_show", templateVars);
 });
 
+//DELETE
 app.post("/urls/:id/delete", (req, res) => {
-
   urlDatabase.find( x => x.id == req.params.id).long = null;
   urlDatabase.find( x => x.id == req.params.id).id = null;
   res.redirect("/urls");
-
 });
 
+//UPDATE
 app.post("/urls/:id/update", (req, res) => {
-
   let foundUrl = urlDatabase.find( x => x.id == req.params.id)
   foundUrl.long = req.body.updURL;
-
   res.redirect(`/urls/${foundUrl.id}`);
-
 });
 
+//SETTING A SHORT URL ADDRESS
 app.get("/u/:shortURL", (req, res) => {
   let id = req.params.shortURL;
   let longURL = urlDatabase.find( x => x.id == id).long
   res.redirect(longURL);
 });
 
+//ROOT PAGE
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-
+//INITIATE SERVER
 app.listen(PORT, () => {
   console.log(`TinyApp listening on port ${PORT}!`);
 });
