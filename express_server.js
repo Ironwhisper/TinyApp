57e0fb399a25eb85cc77c39aeafd78f65057ae03
vagrant <stan.solomean@gmail.com>
2019-01-20 03:02:07 +0000

    added more comments

--- express_server.js
+++ express_server.js
@@ -16,11 +16,11 @@ app.use(cookieSession({
   maxAge: 24 * 60 * 60 * 1000
 }));
 
-//
+//Emmpty database of urls, to be filled out by the user
 let urlDatabase = [];
-
+//Cookies. To be filled out as users register and login.
 let users = {};
-
+//Function used to come up with a unique 6 symbol id
 function generateRandomString() {
   function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1) ) + min;
@@ -33,9 +33,8 @@ function generateRandomString() {
   return result;
 }
 
-//URLS_INDEX
+//urls_index page get request code
 app.get("/urls", (req, res) => {
-
   function urlsForUser(id) {
     let userURLS = [];
       urlDatabase.forEach(function (x) {
@@ -53,7 +52,7 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-//URLS_NEW
+//urls_new page get request code
 app.get("/urls/new", (req, res) => {
   const templateVars = {
     guest: req.session.guest,
@@ -65,7 +64,7 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new", templateVars);
 });
 
-//Make new url in database
+//Make a new short url and store in database
 app.post("/urls", (req, res) => {
   let randomId = generateRandomString();
   if (req.body.longURL.substring(0, 8) === "https://" || req.body.longURL.substring(0, 7) === "http://") {
@@ -75,9 +74,8 @@ app.post("/urls", (req, res) => {
   res.redirect(target_url);
 });
 
-//URLS_SHOW
+//urls_show page get request code, shows more info about a url
 app.get("/urls/:id", (req, res) => {
-
   function urlsForUser(id) {
     let userURLS = [];
       urlDatabase.forEach(function (x) {
@@ -88,7 +86,7 @@ app.get("/urls/:id", (req, res) => {
     return userURLS;
   }
   let userURLNEW = urlsForUser(req.params.id);
-
+  //function used to determine if a url starts with an http(s)://
   function isValidURL(urlArr) {
     for (let i of urlArr) {
       if (i === undefined ||
@@ -99,23 +97,20 @@ app.get("/urls/:id", (req, res) => {
       else return true;
     }
   }
-
   if (isValidURL(userURLNEW) === false || isValidURL(userURLNEW) === undefined) {
     res.send("NOT A VALID URL!");
     return;
   }
-
   let templateVars = {
     shortURL: req.params.id,
     urls: userURLNEW,
     guest: req.session.guest,
     users: Object.values(users)
   };
-
   res.render("urls_show", templateVars);
 });
 
-//DELETE
+//code for deleting a url from the database
 app.post("/urls/:id/delete", (req, res) => {
   let urlCurrent = urlDatabase.find( x => x.id == req.params.id);
   if (urlCurrent.userID !== req.session.guest) {
@@ -127,7 +122,7 @@ app.post("/urls/:id/delete", (req, res) => {
   res.redirect("/urls");
 });
 
-//UPDATE
+//updating an existing url in the database
 app.post("/urls/:id/update", (req, res) => {
   let urlCurrent = urlDatabase.find( x => x.id == req.params.id);
   if (urlCurrent.userID !== req.session.guest) {
@@ -138,16 +133,14 @@ app.post("/urls/:id/update", (req, res) => {
   res.redirect(`/urls/${urlCurrent.id}`);
 });
 
-//SETTING A SHORT URL ADDRESS
+//setting a shortened URL to an original long address
 app.get("/u/:shortURL", (req, res) => {
   let id = req.params.shortURL;
   let longURL = urlDatabase.find( x => x.id == id).long;
   res.redirect(longURL);
 });
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-//REGISTER PAGE
+//user registration page get request code
 app.get("/register", (req, res) => {
   let templateVars = {
     guest: req.session.guest
@@ -155,13 +148,11 @@ app.get("/register", (req, res) => {
   res.render("urls_register", templateVars);
 });
 
-//REGISTER POST
+//user registration page post request code
 app.post("/register", (req, res) => {
-
   const pass = req.body.password;
   const hashedP = bcrypt.hashSync(pass, 10);
   const userID = generateRandomString();
-
   if (!req.body.email || !req.body.password) {
     res.status(400).send('Invalid email or password.');
     return;
@@ -171,7 +162,6 @@ app.post("/register", (req, res) => {
     res.status(400).send("That email is already in use!");
     return;
   }
-
   else users.userID = {
     id : userID,
     email : req.body.email,
@@ -180,17 +170,16 @@ app.post("/register", (req, res) => {
   res.redirect("/login");
 });
 
-//LOGIN PAGE
+//user login page get request code
 app.get("/login", (req, res) => {
   let templateVars = {
     guest: req.session.guest
   };
   console.log(Object.values(users).find(x => x.id === req.session.guest))
-
   res.render("urls_login", templateVars);
 });
 
-//LOGIN POST
+//user login page post request code
 app.post("/login", (req,res) => {
   if (!req.body.email || !req.body.password) {
     res.status(400).send('Empty email or password fields.');
@@ -207,27 +196,21 @@ app.post("/login", (req,res) => {
     return;
   }
   req.session.guest = userBeing.id;
-  // res.cookie("guest", userBeing.id);
   res.redirect("/urls");
 });
 
-//LOGOUT
+//user logout code
 app.post("/logout", (req, res) => {
   delete req.session.guest;
   res.redirect("/urls");
 });
 
-//ROOT PAGE
+//root page
 app.get("/", (req, res) => {
   res.redirect("/urls");
 });
 
-
-//INITIATE SERVER
+//server initialisation
 app.listen(PORT, () => {
   console.log(`TinyApp listening on port ${PORT}!`);
 });
-
-// What would happen if a client requests a non-existent shortURL?
-// What happens to the urlDatabase when the server is restarted?
-// Should your redirects be 301 or 302 - What is the difference?
